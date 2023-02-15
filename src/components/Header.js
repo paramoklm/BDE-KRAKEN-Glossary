46c4117ae6754bffe6378ad8392f971df2be57b5
Param Dave <param.dave@epita.fr>
2023-02-15 19:45:33 +0100

    add tailwind, header position

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import Navigation from './Navigation';
 
 const Header = () => {
     return (
-        <div className='header'>
+        <div className='header shadow-xl w-full top-0 left-0'>
             <Navigation />
         </div>
     );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import Header from '../components/Header';
+import Introduction from '../components/Introduction';
 
 const Home = () => {
     return (
         <div className='accueil'>
             <Header />
-            <h1>Accueil</h1>
-            <h2>Introduction de Kraken le meilleur BDE EPITA !</h2>
+            <Introduction />
         </div>
     );
 };
