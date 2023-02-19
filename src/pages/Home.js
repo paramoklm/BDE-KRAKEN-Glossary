237bcaf01ef0f0a54bc07e6897e22d2cdb82ae31
Param Dave <param.dave@epita.fr>
2023-02-19 11:06:37 +0100

    add victor, slideshow org

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Header from '../components/Header';
 import Introduction from '../components/Home/Introduction';
-
+import Slideshow from '../components/Home/Slideshow';
 const Home = () => {
     return (
         <div className='accueil bg-gradient-to-r from-purple-800 via-fuchsia-700 to-orange-800'>
             <Header />
             <Introduction />
+            <Slideshow/>
         </div>
     );
 };
