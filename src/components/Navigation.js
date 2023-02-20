6d6e7cab3436aa27fc4ba081c6fa7833c96ff6da
Param Dave <param.dave@epita.fr>
2023-02-20 02:19:25 +0100

    nav button resize

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -6,19 +6,19 @@ const Navigation = () => {
         <div className="navigation">
             <ul>
                 <NavLink to="/" className={(nav) => (nav.isActive ? "nav-active" : "")}>
-                    <li className='hover:bg-blue-400 px-1 font-bold bg-gradient-to-r from-purple-800 via-fuchsia-700 to-purple-800 rounded'>Accueil</li>
+                    <li className='text-xs four:text-sm hover:bg-blue-400 px-1 font-bold bg-gradient-to-r from-purple-800 via-fuchsia-700 to-purple-800 rounded'>Accueil</li>
                 </NavLink>
                 <NavLink to="/membres" className={(nav) => (nav.isActive ? "nav-active" : "")}>
-                    <li className='hover:bg-blue-400 px-1 font-bold bg-gradient-to-r from-purple-800 via-fuchsia-700 to-purple-800 rounded'>Membres</li>
+                    <li className='text-xs four:text-sm hover:bg-blue-400 px-1 font-bold bg-gradient-to-r from-purple-800 via-fuchsia-700 to-purple-800 rounded'>Membres</li>
                 </NavLink>
                 <NavLink to="/planning" className={(nav) => (nav.isActive ? "nav-active" : "")}>
-                    <li className='hover:bg-blue-400 px-1 font-bold bg-gradient-to-r from-purple-800 via-fuchsia-700 to-purple-800 rounded'>Planning</li>
+                    <li className='text-xs four:text-sm hover:bg-blue-400 px-1 font-bold bg-gradient-to-r from-purple-800 via-fuchsia-700 to-purple-800 rounded'>Planning</li>
                 </NavLink>
                 <NavLink to="/events" className={(nav) => (nav.isActive ? "nav-active" : "")}>
-                    <li className='hover:bg-blue-400 px-1 font-bold bg-gradient-to-r from-purple-800 via-fuchsia-700 to-purple-800 rounded'>Events</li>
+                    <li className='text-xs four:text-sm hover:bg-blue-400 px-1 font-bold bg-gradient-to-r from-purple-800 via-fuchsia-700 to-purple-800 rounded'>Events</li>
                 </NavLink>
                 <NavLink to="/cartes" className={(nav) => (nav.isActive ? "nav-active" : "")}>
-                    <li className='hover:bg-blue-400 px-1 font-bold bg-gradient-to-r from-purple-800 via-fuchsia-700 to-purple-800 rounded'>Cartes</li>
+                    <li className='text-xs four:text-sm hover:bg-blue-400 px-1 font-bold bg-gradient-to-r from-purple-800 via-fuchsia-700 to-purple-800 rounded'>Cartes</li>
                 </NavLink>
             </ul>
         </div>
