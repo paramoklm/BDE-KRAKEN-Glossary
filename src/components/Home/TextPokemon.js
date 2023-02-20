3dc0f5ab6c70c208199192cad439871040b2cb74
Param Dave <param.dave@epita.fr>
2023-02-20 17:56:41 +0100

    23 to 22 cartes pokemon

--- src/components/Home/TextPokemon.js
+++ src/components/Home/TextPokemon.js
@@ -5,7 +5,7 @@ const TextPokemon = () => {
         <div className='sigmar h-full w-5/6 flex items-center justify-center
                                 text-xs four:text-sm five:text-lg six:text-2xl seven:text-4xl nine:text-5xl 
                                 whitespace-pre whitespace-pre-wrap break-normal text-center italic'>
-            Des lots en trouvant les 23 cartes !
+            Des lots en trouvant les 22 cartes !
         </div>
     );
 };
