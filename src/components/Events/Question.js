c48534789c16c0ec3a4ab30a8d12323e7e631f05
Param Dave <param.dave@epita.fr>
2023-02-22 22:54:50 +0100

    update pokemon, question krak event img

--- src/components/Events/Question.js
+++ src/components/Events/Question.js
@@ -3,9 +3,12 @@ import React from 'react';
 const Question = () => {
     return (
         <div className='border-double border-8 border-purple-500 rounded-lg bg-black bg-opacity-70 w-5/6 mx-auto my-3'>
+                <div className='flex justify-center mx-5 my-5'>
+                    <img className='rounded h-1/2 w-1/2' src={require('./../../assets/img/question-1.png')} alt="question-1" />
+                </div>
                 <div className='flex justify-center'>
                     <ul className='relative inline-grid grid-rows-1 flex justify-center gap-4'>
-                        <img className='rounded' src={require('./../../assets/img/question.png')} alt="question" />
+                        <img className='rounded ' src={require('./../../assets/img/question-2.png')} alt="question-2" />
                     </ul>
                 </div>
         </div>
