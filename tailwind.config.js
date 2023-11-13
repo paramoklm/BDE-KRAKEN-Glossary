87a35523b6d58deb60948dbee23f83919c12f111
Param Dave <param2001.dave@gmail.com>
2023-11-13 15:11:01 +0100

    feat(config): add tailwind config js

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    screens: {
+      'two': '200px',
+      'three': '300px',
+      'four': '400px',
+      'five': '500px',
+      'six': '600px',
+      'seven': '700px',
+      'eight': '800px',
+      'nine': '900px',
+      'ten': '1000px',
+      'eleven': '1100px',
+      'twelve': '1200px',
+      'thirteen': '1300px'
+    },
+    extend: {},
+    fontFamily: {
+      cursive: ['Lobster', 'Press Start 2P', 'Sigmar One']
+    }
+  },
+  plugins: [],
+}
